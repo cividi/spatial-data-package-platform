fd15f6db64ab6d1ccb3b336c54a6b60b9a442942
Yves Serrano <ys@taywa.ch>
2019-12-27 15:15:44 +0100

    removed comments in fastapi/main/__init__.py

--- fastapi/main/__init__.py
+++ fastapi/main/__init__.py
@@ -5,11 +5,3 @@ from .graph import Query
 
 app = FastAPI()
 app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str = None):
-#     return {"item_id": item_id, "q": q}
