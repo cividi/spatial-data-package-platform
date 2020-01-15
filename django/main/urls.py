cca2066a28c9e478bf1d74ac251a12025762c675
Yves Serrano <ys@taywa.ch>
2020-01-15 15:32:13 +0100

    initial vue frontend setup and docker image

--- django/main/urls.py
+++ django/main/urls.py
@@ -12,4 +12,5 @@ if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+        document_root=settings.MEDIA_ROOT
+    )
