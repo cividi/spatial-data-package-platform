2f8a620d2f884ea8897198767ca7ce70c569dc9b
Yves Serrano <ys@taywa.ch>
2020-01-16 13:02:48 +0100

    some jslint formatting, jslint config update

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/airbnb'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: [
+      'error',
+      'single'
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    'func-names': 'off',
+    'no-param-reassign': 'off',
+    'no-new': 'off',
+    'no-shadow': 'off',
+    'prefer-destructuring': 'off',
+    'no-prototype-builtins': 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint',
-  },
+    parser: 'babel-eslint'
+  }
 };
