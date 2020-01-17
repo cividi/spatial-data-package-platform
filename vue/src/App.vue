a33d12bdbb04a99c64f240353213432b119a8cc9
Roman Abt <ra@taywa.ch>
2020-01-17 16:01:20 +0100

    basic styles and html

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="app">
-    <router-link to="/">
-      <img alt="gemeindescan logo" height="50" src="@/assets/images/gemeindescan-logo.svg">
-    </router-link>
-    <div id="nav">
-      <router-link to="/about">Hintergrund</router-link>
-      <router-link to="/imprint">Impressum</router-link>
-      <router-link to="/contact">Kontakt</router-link>
-    </div>
-    <router-view/>
+    <header id="topbar">
+      <router-link id="logo" to="/">
+        <img alt="gemeindescan logo" height="50" src="@/assets/images/gemeindescan-logo.svg">
+      </router-link>
+      <nav id="mainnav">
+        <router-link to="/about">Hintergrund</router-link>
+        <router-link to="/imprint">Impressum</router-link>
+        <router-link to="/contact">Kontakt</router-link>
+      </nav>
+      <div id="useractions" class="small">
+        Login
+        Kostenlos Account erstellen
+      </div>
+    </header>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <style>
 #app {
 }
-#nav {
-  padding: 30px;
+#topbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.2em 2.4em;
+  border-bottom: 1px solid #e3e3e3;
+}
+#mainnav {
 }
 
-#nav a {
+#mainnav a {
   font-weight: bold;
   color: #2c3e50;
-  margin-right: 10px;
+  margin: 0 0.5em;
 }
 
-#nav a.router-link-exact-active {
+#mainnav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="center">
     <img alt="gemeindescan logo" height="400" src="@/assets/images/gmdscn-ch-map.svg">
+    <h1>Entdecke das Lebensraum-Potential deiner Gemeinde.</h1>
+    <p>At delicatissimi pro affert iuvaret ex qui vulputate id cu altera
+      eu philosophia lorem rebum nibh adhuc. Et persequeris tantas
+      theophrastus saperet ne ex instructior doctus altera.</p>
   </div>
 </template>
 
 <script>
+
 </script>
