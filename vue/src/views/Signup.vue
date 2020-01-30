f7b6ce970f02477ad06d1036c2cc82d67aab4faf
Yves Serrano <ys@taywa.ch>
2020-01-30 15:28:28 +0100

    just use iframe to mount the typeform, more robust

--- vue/src/views/Signup.vue
+++ vue/src/views/Signup.vue
@@ -7,13 +7,9 @@
           <v-row justify="center">
               <v-col sm="12" md="8" lg="6">
                 <h1>Account erstellen</h1>
-                <div class="typeform-widget" :data-url="dataurl"
-                  data-transparency="50" style="width: 100%; height:500px"></div>
-                <div style="font-family: Sans-Serif;font-size: 12px;
-                  color: #999;opacity: 0.5; padding-top: 5px;">
-                <!-- eslint-disable-next-line max-len -->
-                <a href="https://admin.typeform.com/signup?utm_campaign=vjVoPr&amp;.utm_source=typeform.com-01DH3Y9W4K4NQZANA3FZ4EVVMZ-essentials&amp;.utm_medium=typeform&amp;.utm_content=typeform-embedded-poweredbytypeform&amp;.utm_term=EN"
-                  style="color: #999" target="_blank"></a>
+                <div style="padding-top:20px;">
+                  <iframe width="100%" height="500" :src="dataurl" frameborder="0">
+                  </iframe>
                 </div>
               </v-col>
           </v-row>
@@ -32,19 +28,14 @@ export default {
   computed: {
     dataurl() {
       if (this.$route.params.gemeinde) {
-        return `https://cividi.typeform.com/to/vjVoPr?gemeinde=${this.$route.params.gemeinde}`;
+        return `//cividi.typeform.com/to/vjVoPr?bfs_gmdno=${this.$route.params.gemeinde}`;
       }
-      return 'https://cividi.typeform.com/to/vjVoPr';
+      return '//cividi.typeform.com/to/vjVoPr';
     }
   },
 
   props: {
     gemeinde: String
-  },
-
-  mounted() {
-    // eslint-disable-next-line
-    (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
   }
 };
 </script>
