bc213f5e919c09fac0558131f26722bf0e6dd61f
Yves Serrano <ys@taywa.ch>
2020-01-31 12:06:40 +0100

    added content and cividi and smartuse logos

--- vue/src/views/Contact.vue
+++ vue/src/views/Contact.vue
@@ -4,7 +4,32 @@
         wrap
       >
         <v-flex>
-          <h1>This is the contact page</h1>
+          <h1>
+            Kontakt
+          </h1>
+          <p>
+            Direkt mit dem Gemeindescan-Team in Kontakt treten:<br>
+            <a href="mailto:kontakt@gemeindescan.ch">kontakt@gemeindescan.ch</a><br>
+            <a href="tel:+41 43 543 44 45">+41 43 543 44 45</a><br>
+            Flüelastrasse 10<br>
+            8048 Zürich
+          </p>
+          <p>
+            Die Gemeindescan-Plattform ist Open Source und Open Development.
+            Zum Gemeindescan beitragen: https://bitbucket.org/cividi/gemeindescan-webui/
+          </p>
+          <p>
+            Gemeindescan ist Teil der Smartuse-Produktfamilie von cividi.
+          </p>
+          <p>
+            cividi - digitale Werkzeuge für die analoge Stadt.
+          </p>
+          <a href="https://www.cividi.ch" target="_blank" style="padding-right:40px">
+            <img src="@/assets/images/logo-cividi.svg" width="250px">
+          </a>
+          <a href="https://www.smartuse.ch" target="_blank">
+            <img src="@/assets/images/logo-smartuse.svg" width="250px">
+          </a>
         </v-flex>
     </v-layout>
   </v-container>
--- vue/src/views/Imprint.vue
+++ vue/src/views/Imprint.vue
@@ -4,7 +4,142 @@
         wrap
       >
         <v-flex>
-          <h1>This is an imprint page</h1>
+          <h1>
+            Impressum
+          </h1>
+          <p>
+            cividi<br>
+            ℅ Smartuse GmbH<br>
+            Flüelastrasse 10<br>
+            8048 Zürich<br>
+            <a href="mailto:legal@gemeindescan.ch" target="_blank">legal@gemeindescan.ch</a>
+          </p>
+
+          <h2>
+            Datenschutzerklärung
+          </h2>
+          <p>
+            Diese Webseite und ihr Inhalt wird durch <em>cividi GmbH</em> gestellt.
+            Der Schutz Ihrer persönlichen Daten ist uns wichtig und wir behandeln
+            personenbezogenen Daten vertraulich und entsprechend der gesetzlichen
+            Datenschutzvorschriften sowie dieser Datenschutzerklärung.
+            Die Nutzung unserer Webseite verlangt generell keine Angabe persönlicher Daten.
+            Personenbezogene Daten wie Namen oder E-Mail-Adressen werden nur auf freiwilliger
+            Basis gesammelt, so zum Beispiel bei dem Ausfüllen unserer Formularen oder bei der
+            Anmeldung für unseren Newsletter.
+            Bei der Nutzung unserer Webseite werden aber automatisch Informationen allgemeiner
+            Natur erfasst.
+            Diese Informationen werden im Server-Logfile erfasst und beinhalten die Art des
+            Webbrowsers, das verwendete Betriebssystem, den Domainnamen Ihres
+            Internet-Service-Providers, Ihre IP-Adresse und ähnliches.
+            Eure Daten werden dabei jedoch nicht verwendet,
+            um Rückschlüsse auf Ihre Person zu ziehen.
+            Informationen dieser Art werden lediglich statistisch ausgewertet,
+            um unseren Web-Auftritt analysieren und optimieren zu können.
+            Die Daten werden gelöscht, sobald diese für den Zweck der Erhebung nicht mehr
+            erforderlich sind.
+            Dies ist für die Daten, die der Bereitstellung der Webseite dienen,
+            grundsätzlich der Fall, wenn die jeweilige Sitzung beendet ist.
+          </p>
+
+          <h2>
+            Formulare und Newsletterdaten
+          </h2>
+          <p>
+            Wenn Sie unser Formular ausfüllen, werden Ihre Daten sprich
+            E-Mail Adresse, Organisation, Ort, Name, Zweck eurem Besuch sowie Zeitpunkt
+            der Anmeldung bei Typeform und Mailchimp gespeichert.
+            Diese Daten nutzen wir einzig für den von euch angeforderten Newsletter-Versand
+            und um Ihre Anfragen direkt beantworten zu können.
+            Sie können sich mittels Abmelde-Link im Newsletter jederzeit wieder
+            vom Newsletter abmelden.
+            Die Newsletter-Anmeldung erfolgt via Double-Opt-In, sprich Sie erhalten nach Ausfüllen
+            des Anmeldeformulars zwecks Verifizierung eine Bestätigungs-E-Mail.
+            Erst danach werden Sie in die Newsletter-Datenbank übernommen.
+          </p>
+
+          <h2>
+            MailChimp
+          </h2>
+          <p>
+            Unsere Newsletter werden mit „MailChimp” versendet
+            (Newsletterversandplattform von der Rocket Science Group,
+            LLC, 675 Ponce De Leon Ave NE, Suite 5000, Atlanta, GA 30308, USA).
+            Die gesammelten E-Mail-Adressen werden entsprechend auf den MailChimp-Servern
+            gespeichert.
+            MailChimp verwendet diese Informationen für von uns ausgelöste Newsletter-Versände
+            und -Auswertungen. MailChimp nutzt die Daten für Optimierung des eigenen Services,
+            jedoch nicht um Newsletter-Empfänger selber anzuschreiben oder deren Daten
+            an Dritte weiterzugeben.
+            Die Datenschutzbestimmungen von MailChimp könnt ihr
+            <a href="https://mailchimp.com/legal/" target="_blank">hier</a> nachlesen.
+          </p>
+
+          <h2>
+            Typeform
+          </h2>
+          <p>
+            Unsere Formulare werden mit Typeform (C/ Bac de Roda 163, Local 1, Barcelona,
+            Catalunya 08018, ES) entwickelt.
+            Diese Daten nutzen wir einzig für die Weiterentwicklung und Personalisierung
+            unsere Dienstleistungen und Produkten anhand von den von Ihnen angegeben Daten.
+            Die gesammelten E-Mail-Adressen und Antworten werden entsprechend auf den
+            Typeform-Servern gespeichert. Typeform verwendet diese Informationen
+            für die Sammlung der Antworten in von uns ausgelösten Formulare und ihre
+            Auswertungen. Typeform nutzt die Daten für Optimierung des eigenen Services,
+            jedoch nicht um Formular-Ausfüller selber anzuschreiben oder deren
+            Daten an Dritte weiterzugeben.
+            Die Datenschutzbestimmungen von Typeform könnt ihr
+            <a href="https://admin.typeform.com/to/L9Crcj" target="_blank">hier</a> nachlesen.
+          </p>
+
+          <h2>
+            Google Analytics
+          </h2>
+          <p>
+            Diese Website benutzt Google Analytics, einen Webanalysedienst der Google LLC,
+            1600 Amphitheatre Parkway, Mountain View, CA 94043 USA (nachfolgend: „Google”).
+            Google Analytics verwendet sog. „Cookies”, also Textdateien, die auf eurem
+            Computer gespeichert werden und die eine Analyse eurer Benutzung der Webseite
+            ermöglichen. Die durch das Cookie erzeugten Informationen über eure Benutzung
+            unserer Webseite werden in der Regel an einen Server von Google in den USA
+            übertragen und dort gespeichert. Aufgrund der Aktivierung der IP-Anonymisierung,
+            wird eure IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der
+            Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den
+            Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die
+            volle IP-Adresse an einen Server von Google in den USA übertragen und dort
+            gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese
+            Informationen benutzen, um eure Nutzung der Webseite auszuwerten,
+            um Reports über die Webseitenaktivitäten zusammenzustellen und um weitere
+            mit der Webseitenutzung und der Internetnutzung verbundene Dienstleistungen
+            gegenüber dem Webseitenbetreiber zu erbringen. Die im Rahmen von Google Analytics
+            von eurem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google
+            zusammengeführt.
+          </p>
+
+          <h2>
+            SSL-Verschlüsselung
+          </h2>
+          <p>
+            Um die Sicherheit eurer Daten bei der Übertragung zu schützen,
+            verwenden wir dem aktuellen Stand der Technik entsprechende Verschlüsselungsverfahren
+            (z. B. SSL) über HTTPS.
+          </p>
+
+          <h2>
+            Sonstiges
+          </h2>
+          <p>
+            Wir können diese Datenschutzerklärung jederzeit ohne Vorankündigung anpassen.
+            Es gilt die jeweils aktuelle, auf dieser Website publizierte Fassung.
+          </p>
+          <p>
+            Für datenschutzrelevante Anliegen könnt ihr euch wenden an:<br>
+            Viktoria Molnar<br>
+            Flüelastrasse 10<br>
+            8048 Zürich<br>
+            <a href="mailto:legal@cividi.ch">legal@cividi.ch</a>
+          </p>
         </v-flex>
     </v-layout>
   </v-container>
