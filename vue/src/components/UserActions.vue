351208c6347f7e8f52eeb2fe9cc473e414be6e5d
Yves Serrano <ys@taywa.ch>
2020-02-20 08:05:04 +0100

    added missing UserActions component

---
+++ vue/src/components/UserActions.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable -->
+<i18n>
+{
+  "de": {
+    "loginText": "Login",
+    "signupText": "Unverbindliche Anfrage"
+  },
+  "fr": {
+    "loginText": "Login",
+    "signupText": "Demande sans engagement"
+  }
+}
+</i18n>
+<!-- eslint-enable -->
+
+<template>
+  <div>
+    <v-btn small text color="primary">
+      <router-link :to="'/' + $i18n.locale + '/login'">{{ $t('loginText') }}</router-link>
+    </v-btn>
+    <v-btn small outlined color="primary">
+      <router-link key="signup" :to="'/' + $i18n.locale + '/signup'">
+        {{ $t('signupText') }}
+      </router-link>
+    </v-btn>
+  </div>
+</template>
+
+<style>
+</style>
