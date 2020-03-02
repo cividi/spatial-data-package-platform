04d51180198718d0b2c184fcf4781abf5cecea1c
Yves Serrano <ys@taywa.ch>
2020-03-02 12:21:13 +0100

    added SnapshotMeta and SnapshotList components, updated graphql query to get related snapshots

---
+++ vue/src/components/SnapshotMeta.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SnapshotMeta',
+  data() {
+    return {};
+  },
+  props: {
+    title: String,
+    description: String
+  }
+};
+</script>
