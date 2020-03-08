0c3e4ce57a8b9441b7dab24231c527d46e6a177b
Yves Serrano <ys@taywa.ch>
2020-03-08 09:37:37 +0100

    display legend from datapackage

--- vue/src/components/SnapshotMeta.vue
+++ vue/src/components/SnapshotMeta.vue
@@ -2,22 +2,33 @@
   <div>
     <div class="smaller">
       <h3>{{ title }}</h3>
+      <p >
+        <a class="legend--hash" :href="'https://gemeindescan.ch/' + hash" target="_blank">
+        gemeindescan.ch/{{ hash }}
+        </a>
+      </p>
       <p>{{ description }}</p>
     </div>
     <v-list
     dense
     class="legend">
       <v-list-item
-        v-for="(legend, i) in legends"
+        v-for="(item, i) in legend"
         :key="i"
         class="pa-0 mb-1"
         >
-        <v-list-item-icon class="my-0 mr-4">
-          <v-icon :color="legend.color">mdi-circle</v-icon>
+        <v-list-item-icon class="my-0 mr-2">
+          <v-icon v-if="item.shape === 'circle'"
+            :color="item.color" :style="{ fontSize: item.size + 'em'}">mdi-circle</v-icon>
+          <v-icon v-else-if="item.shape === 'square'"
+            :color="item.color" :style="{ fontSize: item.size + 'em'}">mdi-square</v-icon>
+          <span v-else-if="item.shape === 'line'" class="legend--line"
+            :style="{ backgroundColor: item.color, height: 0.5 * item.size + 'em'}">&nbsp;
+          </span>
         </v-list-item-icon>
         <v-list-item-content class="py-0">
           <v-list-item-title>
-            {{legend.title}}
+            {{ item.label }}
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
@@ -31,23 +42,31 @@
 .legend.v-list--dense .v-list-item {
   min-height: 24px;
 }
+.v-list-item__icon {
+  min-width: auto;
+}
+.legend--line {
+  margin: auto 0;
+  width: 2em;
+}
+.legend--hash,
+.legend--hash:visited {
+  color: gray;
+  font-weight: bold;
+}
 </style>
 
 <script>
 export default {
   name: 'SnapshotMeta',
   data() {
-    return {
-      legends: [
-        { title: 'Legende 1', color: '#9CF' },
-        { title: '', color: '#69C' },
-        { title: 'Letzte Legende', color: '#369' }
-      ]
-    };
+    return {};
   },
   props: {
+    hash: String,
     title: String,
-    description: String
+    description: String,
+    legend: Array
   }
 };
 </script>
