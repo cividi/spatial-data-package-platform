3208a0f9466f7a24a9f70eba10cf6a74375efac7
Yves Serrano <ys@taywa.ch>
2020-04-29 23:12:27 +0200

    use generic pool for headless browser

--- vue/screenshot-service/screenshot-server.js
+++ vue/screenshot-service/screenshot-server.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,20 +11,34 @@ const port = 8079;
 
 const browserFactory = {
   async create() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+      {
+        executablePath: '/usr/bin/chromium-browser',
+        headless: true,
+        devtools: false,
+        args: [
+          '--no-sandbox',
+          '--disable-dev-shm-usage'
+          // '--remote-debugging-port=9222',
+          // '--remote-debugging-address=0.0.0.0'
+        ]
+      }
+    );
     const page = await browser.newPage();
     return page;
   },
-  destroy(puppeteer) {
-    puppeteer.close();
+  async destroy(browser) {
+    await browser.close();
+    exec('pkill chrome');
   }
 };
 
-const browserPagePool = genericPool.createPool(browserFactory, {
+const browserPool = genericPool.createPool(browserFactory, {
   max: 1,
   min: 0,
   maxWaitingClients: 50,
-  idleTimeoutMillis: 30000
+  idleTimeoutMillis: 30000, // 1200000, // 20min
+  evictionRunIntervalMillis: 1000
 });
 
 app.use(cors());
@@ -33,17 +48,6 @@ app.use(bodyParser.json());
 app.get('*', async (req, res) => {
   console.log(req.params);
   const path = req.params['0'];
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser',
-    headless: true,
-    devtools: false,
-    args: [
-      '--no-sandbox',
-      '--disable-dev-shm-usage'
-      // '--remote-debugging-port=9222',
-      // '--remote-debugging-address=0.0.0.0'
-    ]
-  });
 
   let screenshotUrl = `http://localhost:8080${path}`;
   let pageOptions = {
@@ -63,18 +67,20 @@ app.get('*', async (req, res) => {
     };
   }
 
-  const page = await browser.newPage();
+  const page = await browserPool.acquire();
   page.setViewport(pageOptions);
   await page.goto(screenshotUrl);
   await page.waitForSelector('#mapinfo .v-list-item__title');
-  //  dawait page.waitForSelector('.leaflet-control-attribution a');
+  await page.waitFor(1000); // extra 1sec wait, for map tiles
   const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
-  await browser.close();
+  // await browser.close();
+  await browserPool.release(page);
   res.writeHead(200, {
     'Content-Type': 'image/png',
     'Content-disposition': 'attachment;filename=screenshot.png',
     'Content-Length': screenshotBuffer.length
   });
+
   res.end(Buffer.from(screenshotBuffer, 'binary'));
 });
 
