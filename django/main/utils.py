2dab8e0ccc032e1270a3fbe01b2d5d4e391fc575
Yves Serrano <ys@taywa.ch>
2020-05-04 00:20:31 +0200

    added opengraph meta data and screenshots

---
+++ django/main/utils.py
@@ -0,0 +1,15 @@
+
+from sorl.thumbnail.base import ThumbnailBackend, EXTENSIONS
+from sorl.thumbnail.conf import settings
+from sorl.thumbnail.helpers import tokey, serialize
+import os.path
+
+class PermalinkThumbnailBackend(ThumbnailBackend):
+    def _get_thumbnail_filename(self, source, geometry_string, options):
+        """
+        Computes the destination filename.
+        """
+        key = tokey(source.key, geometry_string, serialize(options))
+        filename, _ext = os.path.splitext(os.path.basename(source.name))
+        path = '%s/%s' % (key, filename)
+        return '%s%s.%s' % (settings.THUMBNAIL_PREFIX, path, EXTENSIONS[options['format']])
