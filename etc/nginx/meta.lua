98d942c4b4b76f367ab555a3f311d61b90402d63
Yves Serrano <ys@taywa.ch>
2020-05-16 17:21:15 +0200

    increase body size for bigger snapshots, change snapshot meta save path

--- etc/nginx/meta.lua
+++ etc/nginx/meta.lua
@@ -1,5 +1,5 @@
 
-local file_meta, err = io.open("/var/services/django/snapshot_meta/" .. ngx.var.hash ..".html", "r")
+local file_meta, err = io.open("/var/services/django/media/snapshot-meta/" .. ngx.var.hash ..".html", "r")
 if file_meta then
     local content = file_meta:read("*all")
     return content
