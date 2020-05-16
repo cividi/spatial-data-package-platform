f2a3e847100480ff204ca61288a8e5a5587cb47a
Yves Serrano <ys@taywa.ch>
2020-05-16 15:38:03 +0200

    add meta.lua, create proxy_conf and proxy_meta includes, added replace marker to index.html

---
+++ etc/nginx/meta.lua
@@ -0,0 +1,8 @@
+
+local file_meta, err = io.open("/var/services/django/snapshot_meta/" .. ngx.var.hash ..".html", "r")
+if file_meta then
+    local content = file_meta:read("*all")
+    return content
+else
+    return ""
+end
