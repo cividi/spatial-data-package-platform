7478e619e3910e5402409fa6d94d62bb1e3658a4
Roman Abt <ra@taywa.ch>
2021-10-21 23:23:25 +0200

    fix merge conflict in urls.py

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 # from django.contrib.auth.views import LoginView
-from gsmap.views import CustomLoginView, logout, SnapshotFileUploadView, AnnotationCreateView, AnnotationRateUpView  #, AnnotationPublishView
+from gsmap.views import CustomLoginView, logout, SnapshotFileUploadView, AnnotationCreateView, AnnotationRateUpView, AnnotationPublishView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
