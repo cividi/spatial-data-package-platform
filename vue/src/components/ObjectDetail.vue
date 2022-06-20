33281580ef586520222e28a7a942379821f9be8e
Thorben Westerhuys <thorben@cividi.ch>
2022-06-20 09:27:24 +0200

    WIP object detail overlay

--- vue/src/components/ObjectDetail.vue
+++ vue/src/components/ObjectDetail.vue
@@ -2,37 +2,37 @@
 <i18n>
 {
   "de": {
-    "category":"Kategorie:",
-    "state": "Status:",
-    "constructed": "Erbaut:",
-    "demolished": "Abgerissen:",
+    "category":"Kategorie",
+    "state": "Status",
+    "constructed": "Erbaut",
+    "demolished": "Abgerissen",
     "description": "Architektur",
     "moreinfo": "Abrissgrund",
     "comment": "Geschichten und Erinnerungen"
   },
   "fr": {
-    "category":"Catégorie:",
-    "state": "Statut:",
-    "constructed": "Construit:",
-    "demolished": "Arraché:",
+    "category":"Catégorie",
+    "state": "Statut",
+    "constructed": "Construit",
+    "demolished": "Arraché",
     "description": "Architecture",
     "moreinfo": "Cause de la démolition",
     "comment": "Histoires et souvenirs"
   },
   "en": {
-    "category":"Category:",
-    "state": "Status:",
-    "constructed": "Built:",
-    "demolished": "Demolished:",
+    "category":"Category",
+    "state": "Status",
+    "constructed": "Built",
+    "demolished": "Demolished",
     "description": "Architecture",
     "moreinfo": "Demolition reason",
     "comment": "Stories and memories"
   },
   "it": {
-    "category":"Categoria:",
-    "state": "Stato:",
-    "constructed": "Costruito:",
-    "demolished": "Abbattuto:",
+    "category":"Categoria",
+    "state": "Stato",
+    "constructed": "Costruito",
+    "demolished": "Abbattuto",
     "description": "Architettura",
     "moreinfo": "Motivo della demolizione",
     "comment": "Storie e ricordi"
@@ -58,85 +58,69 @@
         class="pa-4 elevation-6"
       >
         <div class="smaller">
-          <v-carousel v-if="curObj.attachements.length > 0" height="auto" hide-delimiters
-            :show-arrows="curObj.attachements.length > 1" class="my-1">
-            <v-carousel-item eager v-for="(item, i) in curObj.attachements" :key="i"
-              :src="djangobaseurl + '/media/' + item.document"></v-carousel-item>
-          </v-carousel>
+          <div class="topContainer">
+            <v-carousel v-if="curObj.attachements.length > 0"
+              height="400px"
+              hide-delimiters
+              :show-arrows="curObj.attachements.length > 1"
+              class="my-1"
+              >
+              <v-carousel-item eager v-for="(item, i) in curObj.attachements" :key="i"
+                :src="djangobaseurl + '/media/' + item.document"
+                class="requestable-filter"
+                contain></v-carousel-item>
+            </v-carousel>
 
-          <h3>{{ curObj.data.properties.title }}</h3>
-          <h4>{{ curObj.data.properties.subtitle }}</h4>
+            <div class="objectTitle">
+              <h3>{{ curObj.data.properties.title }}</h3>
+              <h4>{{ curObj.data.properties.subtitle }}</h4>
+            </div>
+          </div>
 
-          <p>
-            {{ $t('category') }}
-            <span>
-              {{ curObj.category.name }}
-            </span>
-          </p>
-          <p v-if="curObj.state">
-            {{ $t('state') }}
-            <span>
-              {{ curObj.state.name }}
-            </span>
-          </p>
-          <v-container v-if="
-            curObj.data.properties.constructionYear ||
-            curObj.data.properties.demolitionYear"
-            class="pa-0"
-          >
-            <v-row>
-              <v-col v-if="curObj.data.properties.constructionYear">
-                <p>
-                  {{ $t('constructed') }}
-                  <span>
-                    {{ curObj.data.properties.constructionYear }}
-                  </span>
-                </p>
+          <v-container class="body-1 mainDetails">
+            <v-row class="objectData">
+              <v-col class="px-0 py-0">
+                <span>{{ $t('category') }}</span> {{ curObj.category.name }}
               </v-col>
-              <v-col v-if="curObj.data.properties.demolitionYear">
-                <p>
-                  {{ $t('demolished') }}
-                  <span>
-                    {{ curObj.data.properties.demolitionYear }}
-                  </span>
-                </p>
+            </v-row>
+            <v-row class="objectData">
+              <v-col class="px-0 py-1">
+                <span>{{ $t('constructed') }}</span> {{ curObj.data.properties.constructionYear }}
+              </v-col>
+            </v-row>
+            <v-row class="objectData">
+              <v-col class="px-0 py-1">
+                <span>{{ $t('demolished') }}</span> {{ curObj.data.properties.demolitionYear }}
+              </v-col>
+            </v-row>
+            <v-row class="objectData">
+              <v-col class="px-0 py-1">
+                <span>{{ $t('description') }}</span> {{ curObj.data.properties.description }}
+              </v-col>
+            </v-row>
+            <v-row class="objectData">
+              <v-col class="px-0 py-1">
+                <span>{{ $t('moreinfo') }}</span> {{ curObj.data.properties.moreinfo }}
+              </v-col>
+            </v-row>
+            <v-row  v-if="curObj.category.commentsEnabled">
+              <v-col class="px-0 py-0">
+                <span>{{ $t('comment') }}</span>
+              </v-col>
+            </v-row>
+            <v-row class="objectData" v-if="curObj.data.properties.comment">
+              <v-col class="px-0 py-0">
+                <span v-if="curObj.data.properties.commentAuthor">
+                  {{ curObj.data.properties.commentAuthor }}
+                </span> {{ curObj.data.properties.comment }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="px-0 py-0">
+                <div id="commento"></div>
               </v-col>
             </v-row>
           </v-container>
-
-          <div v-if="curObj.data.properties.description">
-            <h5>{{ $t('description') }}</h5>
-            <p>{{ curObj.data.properties.description }}</p>
-          </div>
-          <div v-if="curObj.data.properties.moreinfo">
-            <h5>{{ $t('moreinfo') }}</h5>
-            <p>{{ curObj.data.properties.moreinfo }}</p>
-          </div>
-
-          <div v-if="enableLikes" class="d-flex align-center justify-end primary--text">
-            <p class="rating">
-              <v-icon color="primary" small>mdi-heart-outline</v-icon>
-              <b style="vertical-align: middle;"> {{ curObj.rating }}</b>
-            </p>
-            <v-btn fab x-small color="white" :disabled="ratingpause"
-              class="primary--text" ref="rateupBtn"
-              @click="rateUp(curObj.pk)">
-              <v-icon small>mdi-heart-plus</v-icon>
-            </v-btn>
-            <v-icon id="addHeart" v-if="ratingpause" small color="primary"
-              :style="cssVars">mdi-heart</v-icon>
-          </div>
-          <div v-if="curObj.category.commentsEnabled">
-            <h3>{{ $t('comment') }}</h3>
-            <div v-if="curObj.data.properties.comment">
-              <p>
-                <span v-if="curObj.data.properties.commentAuthor">
-                  {{ curObj.data.properties.commentAuthor }}: </span>
-                {{ curObj.data.properties.comment }}
-              </p>
-            </div>
-            <div id="commento"></div>
-          </div>
         </div>
       </div>
     </v-slide-x-transition>
@@ -153,6 +137,59 @@
   background: #fff;
   z-index: 1100;
   overflow: auto;
+}
+
+h3, h4 {
+  font-size: 3.5em;
+  width: fit-content;
+  line-height: 1em;
+  text-transform: uppercase;
+  border-bottom: 8px solid #000;
+  margin-bottom: 0.4em;
+}
+
+.v-image__image {
+    background-color: transparent !important;
+    border: 0 !important;
+}
+
+.objectTitle {
+  text-transform: uppercase;
+  z-index: 1;
+}
+
+.topContainer {
+  display: grid;
+  align-items: start;
+  justify-items: left;
+}
+.topContainer > * {
+  grid-area: 1/1/1/1;
+}
+.requestable-filter {
+  filter: grayscale(100%) brightness(110%);
+  opacity: 1 !important;
+}
+.v-application .body-1 {
+  font-family: 'Queue', sans-serif !important;
+  font-weight: bold;
+  line-height: 1.5em;
+}
+.body-1 span {
+  font-family: 'Helvetica Neue LT W05 75 Bold', 'Arial', sans-serif !important;
+}
+.body-1 span::after {
+  content: ":";
+}
+.row.objectData {
+  border-top: 2px solid black;
+}
+.mainDetails {
+  border-bottom: 2px solid black;
+}
+
+#commento {
+  margin-top: 12px;
 }
 
 @media (max-width: 500px) {
@@ -185,7 +222,11 @@ export default {
         const commentoScript = document.createElement('script');
         commentoScript.setAttribute('src', `${this.commentoUrl}/js/commento.js`);
         commentoScript.setAttribute('data-auto-init', false);
+        commentoScript.setAttribute('data-no-fonts', true);
+        commentoScript.setAttribute('data-locale', this.$route.params.lang);
         commentoScript.setAttribute('data-page-id', `/${this.$route.params.wshash}/${this.curObj.pk}/`);
+        commentoScript.setAttribute('data-css-override', '/commento.css');
+        commentoScript.setAttribute('data-hide-deleted', true);
         commentoScript.setAttribute('defer', true);
         document.head.appendChild(commentoScript);
         window.setTimeout(() => {
