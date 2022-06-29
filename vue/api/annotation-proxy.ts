3201bb2d3574dd52aecc384f07b46f72bdea27a6
Thorben Westerhuys <thorben@cividi.ch>
2022-06-29 23:42:45 +0200

    cache set to 5min, wip fetch in workspace

--- vue/api/annotation-proxy.ts
+++ vue/api/annotation-proxy.ts
@@ -74,7 +74,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
       variables
     );
     
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=900, stale-while-revalidate');
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=300, stale-while-revalidate');
     return res.status(200).json({ workspace });
   }
 }
